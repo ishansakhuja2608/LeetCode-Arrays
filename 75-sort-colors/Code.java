8b674aba89bf4e91f05fb5db9f45d5337cfc2eba
Ishan Sakhuja <84624937+ishansakhuja2608@users.noreply.github.com>
2022-06-02 09:34:06 +0530

    Update and rename NOTES.md to Code.java

---
+++ 75-sort-colors/Code.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int curr = 0;
+        int left = 0;
+        int right = nums.length-1;
+        while(curr <= right){
+            if(nums[curr] == 0){
+                int temp = nums[curr];
+                nums[curr] = nums[left];
+                nums[left] = temp;
+                curr++;
+                left++;
+            } else if(nums[curr] == 2){
+                int temp = nums[curr];
+                nums[curr] = nums[right];
+                nums[right] = 2;
+                right--;
+            } else {
+                curr++;
+            }
+        }
+    }
+}
