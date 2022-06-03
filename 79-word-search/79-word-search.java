7c90ce5d79a574d93e516e367d93874804e5223b
Ishan Sakhuja <84624937+ishansakhuja2608@users.noreply.github.com>
2022-06-03 23:41:44 +0530

    Time: 114 ms (87.85%), Space: 42.7 MB (22.74%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,40 +1,27 @@
 class Solution {
-    boolean visited[][];
     public boolean exist(char[][] board, String word) {
-        int rows = board.length;
-        int cols = board[0].length;
-        visited = new boolean[rows][cols];
-        for(int i=0; i<rows; i++){
-            for(int j=0; j<cols; j++){
-                if(word.charAt(0) == board[i][j] && search(i ,j, 0, word, board)){
+        for(int i=0; i<board.length; i++)
+            for(int j=0; j<board[i].length; j++)
+                if(board[i][j] == word.charAt(0) && search(board, i, j, 0, word))  // 0 is the number of characters found
                     return true;
-                }
-            }
-        }
+        
         return false;
     }
     
-    private boolean search(int i, int j, int index, String word, char[][] board) {
-        
-        if(index == word.length()) {
+    private boolean search(char[][] board, int i, int j, int count, String word){
+        if(count == word.length())
             return true;
-        }
         
-        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || word.charAt(index) != board[i][j] || visited[i][j]) {
+        if(i < 0 || i >= board.length || j < 0 || j >= board[i].length || board[i][j] != word.charAt(count))
             return false;
-        }
         
-        visited[i][j] = true;
+        char temp = board[i][j];
+        board[i][j] = ' ';
         
-        if(search(i+1, j, index+1, word, board) || 
-           search(i-1, j, index+1, word, board) || 
-           search(i, j+1, index+1, word, board) || 
-           search(i, j-1, index+1, word, board)) {
-            
-            return true;
-        }
-        visited[i][j] = false;
+        boolean found = search(board, i+1, j, count+1, word) || search(board, i-1, j, count+1, word) ||
+                        search(board, i, j+1, count+1, word) || search(board, i, j-1, count+1, word);
+        board[i][j] = temp;
         
-        return false;
+        return found;
     }
 }
