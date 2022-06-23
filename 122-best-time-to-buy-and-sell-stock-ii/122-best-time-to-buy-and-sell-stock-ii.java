38113b0a5473743d1a3b49331053b72d89b51545
Ishan Sakhuja <84624937+ishansakhuja2608@users.noreply.github.com>
2022-06-23 10:48:22 +0530

    Update 122-best-time-to-buy-and-sell-stock-ii.java

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,11 +1,15 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int res = 0;
+        int maxProfit = 0;
+        // If array is null, the net profit is 0
         if(prices == null || prices.length == 0)
             return 0;
-        for(int i=0;i<prices.length-1;i++)
-            if(prices[i+1] > prices[i])
-                res += prices[i+1] - prices[i];
-        return res;
+        
+        for(int i = 0; i < prices.length - 1; i++) {
+            // If the prices on a particular day is more than the prices on the previous day, then we add the profit to the result
+            if(prices[i] < prices[i+1])
+                maxProfit += prices[i+1] - prices[i]; 
+        }
+        return maxProfit;
     }
 }
