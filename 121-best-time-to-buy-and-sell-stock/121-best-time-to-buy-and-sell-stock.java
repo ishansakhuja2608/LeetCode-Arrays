302696adea66fb90ce66d37028bebbda9240f1f9
Ishan Sakhuja <84624937+ishansakhuja2608@users.noreply.github.com>
2022-06-23 10:43:31 +0530

    Update 121-best-time-to-buy-and-sell-stock.java

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,11 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int buy = Integer.MAX_VALUE;
-        int sell = 0;
-        for(int i=0;i<prices.length;i++){
-            buy = Math.min(buy, prices[i]);
-            sell = Math.max(sell, prices[i]-buy);
+        
+        int buy = Integer.MAX_VALUE; // we have to buy the stock on the day when its value is minimum
+        int maxProfit = 0; // have to sell the stock on such a day that the profit earned could be maimum
+        
+        for(int i = 0; i < prices.length; i++) {
+            buy = Math.min(buy, prices[i]); // minimum of array
+            maxProfit = Math.max(maxProfit, prices[i] - buy); // difference of that minimum with all the elements
         }
-        return sell;
+        return maxProfit;
     }
 }
