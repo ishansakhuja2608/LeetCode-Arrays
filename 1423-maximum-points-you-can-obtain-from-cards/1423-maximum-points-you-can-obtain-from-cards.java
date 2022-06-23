591cf257b30a2788ff0300e2e377592ed6ebcba7
Ishan Sakhuja <84624937+ishansakhuja2608@users.noreply.github.com>
2022-06-23 10:59:07 +0530

    Update 1423-maximum-points-you-can-obtain-from-cards.java

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -1,22 +1,22 @@
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
-        int n = cardPoints.length;
-        int windowSize = n-k;
-        int i = 0, j = 0;
+        int n = cardPoints.length; // length of the array
+        int windowSize = n - k; // the size of the sliding window
         int windowSum = 0;
-        int total = 0;
         int minWindowSum = Integer.MAX_VALUE;
+        int total = 0;
+        int i = 0, j = 0;
         
-        while(i < n && j < n){
-            windowSum += cardPoints[j]; // adding to the window sum
-            total += cardPoints[j]; // total score of all the elements
+        while(i < n && j < n) {
+            total += cardPoints[j];
+            windowSum += cardPoints[j];
             
-            if(j >= windowSize){
-                windowSum -= cardPoints[i++]; // sliding the window, removing the leftmost element
+            if(j >= windowSize) {
+                windowSum -= cardPoints[i++]; // keeping the window size same
             }
             
-            if(j >= windowSize-1) {
-                minWindowSum = Math.min(minWindowSum, windowSum);
+            if(j >= windowSize - 1) {
+                minWindowSum = Math.min(minWindowSum, windowSum); // storing the minimum window sum
             }
             j++;
         }
