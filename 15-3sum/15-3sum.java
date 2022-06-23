57877ff3cfd6ce361343657f2f92000da1dd9f24
Ishan Sakhuja <84624937+ishansakhuja2608@users.noreply.github.com>
2022-06-23 12:07:15 +0530

    Update 15-3sum.java

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,27 +1,37 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        // a+b+c = 0; b+c = -a
         Arrays.sort(nums);
-        List<List<Integer>> res = new LinkedList<>();
+        List<List<Integer>> triplets = new ArrayList();
         
-        for(int i = 0 ;i <nums.length - 2 ; i++) {
+        for(int i = 0; i < nums.length - 2; i++) {
             if(i == 0 || (i > 0 && nums[i] != nums[i-1])) {
-                int lo = i + 1, hi = nums.length - 1, sum = -nums[i];
+                int left = i + 1;
+                int right = nums.length-1;
+                int sum = -nums[i];
                 
-                while(lo < hi) {
-                    if(nums[lo] + nums[hi] == sum) {
-                        res.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+                while(left < right) {
+                    if(nums[left] + nums[right] == sum) {
+                        // Adding that triplet to triplets
+                        triplets.add(Arrays.asList(nums[i], nums[left], nums[right]));
                         
-                        while(lo < hi && nums[lo] == nums[lo+1]) lo++;
-                        while(lo < hi && nums[hi] == nums[hi-1]) hi--;
+                        while(left < right && nums[left] == nums[left+1])
+                            left++; // skipping the same elements on the left ride
                         
-                        lo++;
-                        hi--;
+                        while(left < right && nums[right] == nums[right-1])
+                            right--; // skipping the same elements on the right side
+                        
+                        left++;
+                        right--;
+                        
                     }
-                    else if(nums[lo] + nums[hi] < sum) lo++;
-                    else hi--;
+                    else if(nums[left] + nums[right] < sum)
+                        left++;
+                    else
+                        right--;
                 }
             }
         }
-        return res;
+        return triplets;
     }
 }
